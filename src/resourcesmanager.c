1dcf720b468fb881fe464624bbbe62bdfa9730ff
ether <omnipresent.ether@yandex.ru>
2024-09-20 12:16:04 +1000

    fixed wrong path alloctation size

--- src/resourcesmanager.c
+++ src/resourcesmanager.c
@@ -14,7 +14,7 @@ const char *const rmanager_getdir() { return __current_wd; }
 
 char *rmanager_get_shader_path(const char *shader_name) {
   size_t abs_path_size = (strlen(__current_wd) + strlen(shader_name) + 1 +
-                          strlen(SHADERS_EXTENTION)) *
+                          strlen(SHADERS_EXTENTION) + strlen(SHADERS_DIR)) *
                          sizeof(char);
   char *shader_abs_path = malloc(abs_path_size);
   memset(shader_abs_path, 0, abs_path_size);
@@ -27,9 +27,9 @@ char *rmanager_get_shader_path(const char *shader_name) {
   return shader_abs_path;
 }
 
-shader_t* rmanager_get_shader(GLenum type, const char *name) {
-  char* shader_absolute_path = rmanager_get_shader_path(name);
-  shader_t* shader = shader_create(type, shader_absolute_path);
+shader_t *rmanager_get_shader(GLenum type, const char *name) {
+  char *shader_absolute_path = rmanager_get_shader_path(name);
+  shader_t *shader = shader_create(type, shader_absolute_path);
   free(shader_absolute_path);
   return shader;
 }
